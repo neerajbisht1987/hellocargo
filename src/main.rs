96011faec88c3816736d81db80e5e843bb6fadc8
neerajbisht1987 <bisht.neeraj@gmail.com>
2018-04-05 20:13:35 +0530

    -->handling loop and break

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@ use std::cmp::Ordering;
 
 fn main() {
 	let rndnum=rand::thread_rng().gen_range(1,101);
-	println!("secret num:{}",rndnum);	
+	println!("secret num:{}",rndnum);
+	loop {	
 	let mut val =String::new();
 	println!("put a value");
 	io::stdin().read_line(&mut val).expect("fail to read line");	
@@ -15,7 +16,11 @@ fn main() {
 	match val.cmp(&rndnum){
 	Ordering::Less => println!("Too small!"),
 	Ordering::Greater => println!("Too big!"), 
-	Ordering::Equal => println!("You win!"),
+	Ordering::Equal => {
+println!("You win!");
+	break;
+	}
 	}
 	
+}
 }
