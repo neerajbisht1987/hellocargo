3a5d42a4af3e5f53987012d6d02ef8f3ca43b489
neerajbisht1987 <bisht.neeraj@gmail.com>
2018-04-05 19:56:15 +0530

    -->use of crate in random -->conversion from val to int 	let val :u32=val.trim().parse().expect("Please type a number!"); -->match functionality +use std::cmp::Ordering;
    match val.cmp(&rndnum){
    +	Ordering::Less => println!("Too small!"),
    +	Ordering::Greater => println!("Too big!"),
    +	Ordering::Equal => println!("You win!"),
    +	}match val.cmp(&rndnum){
    +	Ordering::Less => println!("Too small!"),
    +	Ordering::Greater => println!("Too big!"),
    +	Ordering::Equal => println!("You win!"),
    +	}

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
+extern crate rand;
 use std::io;
-
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
+	let rndnum=rand::thread_rng().gen_range(1,101);
+	println!("secret num:{}",rndnum);	
 	let mut val =String::new();
 	println!("put a value");
 	io::stdin().read_line(&mut val).expect("fail to read line");	
     println!("added value {}",val);
+	let val: u32 = val.trim().parse() .expect("Please type a number!");
+
+	match val.cmp(&rndnum){
+	Ordering::Less => println!("Too small!"),
+	Ordering::Greater => println!("Too big!"), 
+	Ordering::Equal => println!("You win!"),
+	}
+	
 }
