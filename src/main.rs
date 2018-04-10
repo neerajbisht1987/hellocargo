7629d3a1f4f51b4f8a28f40ed3beb69ade84f989
Neeraj Bisht <neeraj.bisht@1e.com>
2018-04-10 08:58:07 +0530

    -->change the build path -->taking ownership and borrowing ownership,borrowing mutable ownership

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,47 @@ use std::io;
 use rand::Rng;
 use std::cmp::Ordering;
 
+fn takes_ownership(take_ownership_string :String)
+{
+	println!("Owner transfer string :{}",take_ownership_string);
+}
+
+
+fn borrow_read_ownership(borrowed_read_string:&String)
+{
+	println!("Borrowed read String :{}",borrowed_read_string);	
+	//error:cannot borrow as mutable
+	//borrowed_read_string.push_str(" new addition");
+	
+}
+
+fn borrow_write_ownership(borrow_write_string:&mut String)
+{
+	println!("Borrowed write  String :{}",borrow_write_string);	
+	borrow_write_string.push_str(" new addition");
+	println!("Borrowed write String with Addition :{}",borrow_write_string);	
+		
+}
+
 fn main() {
+	let take_ownership_example=String::from("-->ownership example");
+	takes_ownership(take_ownership_example);
+	
+	//comiple time error "value used here after move"
+	//println!("{}",take_ownership_example);
+	
+	
+	let borrow_read_example = String::from("borrowing read example");
+	borrow_read_ownership(&borrow_read_example);	
+	println!("After Funtion Call: {}",borrow_read_example);
+	
+	let  mut borrow_write_example = String::from("borrowing write example");
+	borrow_write_ownership(& mut borrow_write_example);
+	println!("After Funtion Call: {}",borrow_write_example);
+	
+	
+	
+
 	let rndnum=rand::thread_rng().gen_range(1,101);
 	println!("secret num:{}",rndnum);
 	loop {	
